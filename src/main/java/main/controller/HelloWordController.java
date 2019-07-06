216a0e036d25cad2a1b7ac4775cccdcda91d4b65
HumanJohn <qinxianqiang8888@gmail.com>
2019-07-06 15:10:33 +0800

    修改测试提交

--- src/main/java/main/controller/HelloWordController.java
+++ src/main/java/main/controller/HelloWordController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWordController {
 	@RequestMapping("/ali")
 	public String index() {
-		return "hello 大黑鬼";
+		return "hello jenkins";
 	}
 }
