531b91fb8ee30a8f545b7cb3b5bba86bf2aa63ce
HumanJohn <qinxianqiang8888@gmail.com>
2019-07-06 14:18:08 +0800

    首次上传源码

---
+++ src/main/java/main/controller/HelloWordController.java
@@ -0,0 +1,14 @@
+package main.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class HelloWordController {
+	@RequestMapping("/ali")
+	public String index() {
+		return "hello 大黑鬼";
+	}
+}
